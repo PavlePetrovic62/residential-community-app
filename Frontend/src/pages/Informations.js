1fda5244a68e17394d4095c797d692c1ba8a1477
PavlePetrovic62 <petrovicpavle@gmail.com>
2023-06-17 02:28:53 +0200

    Cleaned up unnecessary code

--- Frontend/src/pages/Informations.js
+++ Frontend/src/pages/Informations.js
@@ -1,19 +1,7 @@
-import classes from "./Informations.module.css";
-
-import InformationList from "../components/InformationsContent/InformationList";
-import MotionCard from "../components/UI/Card";
+import InformationsContent from "../components/InformationsContent/InformationsContent";
 
 const Informations = () => {
-  return (
-    <MotionCard
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
-      className={classes.card}
-    >
-      <InformationList />
-    </MotionCard>
-  );
+  return <InformationsContent />;
 };
 
 export default Informations;
