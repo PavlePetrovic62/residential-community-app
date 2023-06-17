0e8d5ded100d177111e7e13c4eead466eb60b6f6
PavlePetrovic62 <petrovicpavle@gmail.com>
2023-06-17 02:36:08 +0200

    Removed useEffect from context

--- Frontend/src/store/info-context.js
+++ Frontend/src/store/info-context.js
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 
 const InfoContext = React.createContext({
-  informations: [],
+  informations: null,
   infoAreLoading: false,
+  loadingError: null,
+  fetchNotifications: () => {},
 });
 
 export const InfoContextProvider = (props) => {
-  const [informationData, setInformationData] = useState();
+  const [informationData, setInformationData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const transformDate = (dateString) => {
-      const dateArray = dateString.split("T");
-      const date = dateArray[0];
-      const dateFragments = date.split("-");
-      const year = dateFragments[0];
-      const month = dateFragments[1];
-      const day = dateFragments[2];
-      const transformedDate = `${day}.${month}.${year}.`;
-      return transformedDate;
-    };
+  const transformDate = (dateString) => {
+    const dateArray = dateString.split("T");
+    const date = dateArray[0];
+    const dateFragments = date.split("-");
+    const year = dateFragments[0];
+    const month = dateFragments[1];
+    const day = dateFragments[2];
+    const transformedDate = `${day}.${month}.${year}.`;
+    return transformedDate;
+  };
 
-    const fetchNotifications = async () => {
-      setIsLoading(true);
+  const fetchNotifications = useCallback(async () => {
+    if (informationData) {
+      return informationData;
+    }
+
+    setIsLoading(true);
+
+    try {
       const response = await fetch(
         "http://sz-jg-122.onwebapp.io/api/notifications"
       );
@@ -37,13 +45,17 @@ export const InfoContextProvider = (props) => {
       });
       setInformationData(transformedInfo);
       setIsLoading(false);
-    };
-    fetchNotifications();
-  }, []);
+    } catch (error) {
+      setError("Дошло је до грешке, обавештења нису могла да се учитају!");
+      setIsLoading(false);
+    }
+  }, [informationData]);
 
   const contextValue = {
     informations: informationData,
     infoAreLoading: isLoading,
+    loadingError: error,
+    fetchNotifications,
   };
 
   return (
