750bc8d78fd11e75231ca5ce1a455f4a760bc77b
PavlePetrovic62 <petrovicpavle@gmail.com>
2023-06-19 15:57:39 +0200

    Added text splitting logic and loading spinner

--- Frontend/src/components/InformationsContent/SingleInformation.js
+++ Frontend/src/components/InformationsContent/SingleInformation.js
@@ -1,20 +1,40 @@
-import { useContext } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import classes from "./SingleInformation.module.css";
 import InfoContext from "../../store/info-context";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const SingleInformation = () => {
-  const { informations } = useContext(InfoContext);
+  const { informations, infoAreLoading, loadingError } =
+    useContext(InfoContext);
   const params = useParams();
+  const [singleInfoTitleContent, setSingleInfoTitleContent] = useState(null);
+  const [singleInfoTextContent, setSingleInfoTextContent] = useState(null);
 
-  const singleInfo = informations?.find((info) => info.id === params.infoId);
+  useEffect(() => {
+    const singleInfo = informations?.find((info) => info.id === params.infoId);
+    setSingleInfoTitleContent(singleInfo?.title);
+
+    if (singleInfo) {
+      const singleInfoText = singleInfo.text;
+      const formattedInfoText = singleInfoText
+        .split("<br>")
+        .map((str) => <p key={str}>{str}</p>);
+      setSingleInfoTextContent(formattedInfoText);
+    }
+  }, [informations, params.infoId]);
 
   return (
-    <article className={classes.article}>
-      <h1>{singleInfo?.title}</h1>
-      <p>{singleInfo?.text}</p>
-    </article>
+    <Fragment>
+      {informations && !loadingError && (
+        <article className={classes.article}>
+          <h1>{singleInfoTitleContent}</h1>
+          <div>{singleInfoTextContent}</div>
+        </article>
+      )}
+      {infoAreLoading && <LoadingSpinner />}
+    </Fragment>
   );
 };
 
